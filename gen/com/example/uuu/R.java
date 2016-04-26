00508b958e745f2896e98778e245bad786c2c61b
Midoky <605808578@qq.com>
2016-04-27 01:55:00 +0800

    修改了mainactivity的布局按钮，修复了几个BUG

--- gen/com/example/uuu/R.java
+++ gen/com/example/uuu/R.java
@@ -23,25 +23,33 @@ public final class R {
     }
     public static final class drawable {
         public static final int add_c=0x7f020000;
-        public static final int cell_phone=0x7f020001;
-        public static final int circle=0x7f020002;
-        public static final int contacts=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int loading_bg=0x7f020005;
-        public static final int lock=0x7f020006;
-        public static final int mouse=0x7f020007;
-        public static final int p_left=0x7f020008;
-        public static final int p_right=0x7f020009;
-        public static final int qq_icon=0x7f02000a;
-        public static final int secret_bg=0x7f02000b;
+        public static final int add_c1=0x7f020001;
+        public static final int cell_phone=0x7f020002;
+        public static final int circle=0x7f020003;
+        public static final int contact_icon=0x7f020004;
+        public static final int contacts=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int loading_bg=0x7f020007;
+        public static final int lock=0x7f020008;
+        public static final int main_bg=0x7f020009;
+        public static final int main_bgr=0x7f02000a;
+        public static final int mouse=0x7f02000b;
+        public static final int p_left=0x7f02000c;
+        public static final int p_right=0x7f02000d;
+        public static final int qq_icon=0x7f02000e;
+        public static final int secret_bg=0x7f02000f;
+        public static final int setting_icon=0x7f020010;
+        public static final int text_icon=0x7f020011;
+        public static final int wings=0x7f020012;
+        public static final int wings1=0x7f020013;
     }
     public static final class id {
         public static final int about=0x7f080021;
         public static final int action_settings=0x7f080026;
         public static final int bnLogin=0x7f08000b;
-        public static final int button1=0x7f08000e;
-        public static final int button2=0x7f08000d;
-        public static final int button3=0x7f08000f;
+        public static final int button1=0x7f08000d;
+        public static final int button2=0x7f08000f;
+        public static final int button3=0x7f08000e;
         public static final int cText=0x7f08000a;
         public static final int checkBox1=0x7f080017;
         public static final int checkBox2=0x7f080019;
--- src/com/example/uuu/MainActivity.java
+++ src/com/example/uuu/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.main_activity);
+		setContentView(R.layout.main_activity);		
 		Button button1 = (Button) findViewById(R.id.button1);
 		Button button2 = (Button) findViewById(R.id.button2);
 		Button button3 = (Button) findViewById(R.id.button3);
